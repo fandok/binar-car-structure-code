d9bc635b0063f8dd5251584a1b8ff6e33b1daeb0
fandok <fandika.okdiba@gmail.com>
2023-03-25 01:05:25 +0700

    25-03-2023

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
-import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <Detail />
   </React.StrictMode>
 );
--- src/pages/Detail/components/Content/index.jsx
+++ src/pages/Detail/components/Content/index.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Collapse } from "reactstrap";
+
+import iconUp from "../../../../assets/detail/fi_chevron-up.svg";
+import iconPerson from "../../../../assets/detail/fi_users.svg";
+
+import "./style.css";
+
+const DETAIL_URL = "https://bootcamp-rent-cars.herokuapp.com/customer/car/2271";
+const CAPACITY = {
+  small: "2-4",
+  medium: "4-6",
+  large: "6-8",
+};
 
 const Content = () => {
-  return <div>Content</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [contentData, setContentData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(DETAIL_URL);
+      setContentData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      Content
+      {contentData?.image && (
+        <div>
+          <img
+            width={270}
+            height={160}
+            src={contentData?.image}
+            alt={contentData?.name}
+          />
+        </div>
+      )}
+      <div>{contentData?.name}</div>
+      <div>
+        <img src={iconPerson} alt="Person" />
+        <div>{CAPACITY[contentData?.category]} orang</div>
+      </div>
+      <div>
+        {new Intl.NumberFormat("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        }).format(contentData?.price)}
+      </div>
+      <div
+        onClick={() => {
+          setIsOpen((val) => !val);
+        }}
+        style={{
+          minWidth: 347,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <span>Refund, Reschedule, Overtime</span>
+        <img
+          className="iconChevron"
+          data-open={isOpen}
+          src={iconUp}
+          alt="icon-up"
+        />
+      </div>
+      <Collapse isOpen={isOpen}>
+        <div>Tidak termasuk biaya makan sopir Rp 75.000/hari</div>
+      </Collapse>
+    </div>
+  );
 };
 
 export default Content;
